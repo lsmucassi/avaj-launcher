dfb0ed524bde9200f681dab8c33c92e1dc6812dd
Linda MUCASSI <lindasmucassi@gmail.com>
2018-08-24 14:09:07 +0200

    working

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+rm -rf sources.txt
+rm -rf simulation.txt
+rm -rf out/
+rm -rf src/com/avaj/launcher/**/*.class
---
+++ launch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sh process.sh 4
+java -cp src com.avaj.launcher.Simulator $1
